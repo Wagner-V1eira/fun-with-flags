74ccd9e4e0204bd1b5d8311950fa71ce3e600745
Wagner Rosa <wagnervieira.rosa@hotmail.com>
2025-01-14 11:32:02 -0300

    Add Footer and Header components

---
+++ app/components/footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer className="py-6 mb-8">
+      <p className="text-center">Made with 🌪️ Wagner Rosa </p>
+    </footer>
+  );
+}
+
+export default Footer;
--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,13 @@
+import { Footer, Header } from "./components";
+
 export default function Home() {
-  return (  
-    <> 
-    <header>
-      <h1>This is a header</h1>
-    </header>
+  return (
+    <>
+    <Header />
     <main className="flex-1">
-      <h1>This is a main content</h1>
+      This is a main
     </main>
-    <footer>
-      <h1>This is a footer</h1>
-    </footer>
+    <Footer />
     </>
   );
 }
