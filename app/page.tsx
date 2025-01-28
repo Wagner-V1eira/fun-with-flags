26ae97edf41aae0f668535bde556b427a1490787
Wagner Rosa <wagnervieira.rosa@hotmail.com>
2025-01-27 22:26:33 -0300

    Create Api Client

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { useEffect, useState } from "react";
+import { countriesApi } from "./services";
 import { Card, Footer, Grid, Header } from "./components";
 
 type Country = {
@@ -23,18 +24,15 @@ const [ error, setError ] = useState<string | null>(null);
   
   useEffect(() => {
     const fetchCountries = async () => {
-      try {
-      const response = await fetch('https://restcountries.com/v3.1/all?fields=cca3,flags,name,capital,region,population');
-      const data = await response.json();
-      setCountries(data);
-      setLoading(false);
-      } catch (error) {
-        setError("Failed to fetch data");
-        console.log(error);
-      } finally {
-        setLoading(false); 
-      };
+    const [ response, error ] = await countriesApi.getAll();
+    setLoading(false);
+    if (error) {
+      setError(error);
+      return;
     }
+
+    setCountries(response);
+  };
 
     fetchCountries();
   }, []);
